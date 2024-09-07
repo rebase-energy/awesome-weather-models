b61428d814f7185d67eec2899b70d1d87a9a74f3
sebaheg <sebastian@rebase.energy>
2024-09-07 13:31:51 +0200

    Fix convert to table

--- validate_convert_insert.py
+++ validate_convert_insert.py
@@ -23,81 +23,12 @@ def sort_keys(data, schema):
     print("Sorting keys by predefined order.")
     order = list(schema["properties"].keys())
     # Create a new dictionary with keys in the specified order
-    sorted_data = {key: data[key] for key in order if key in data}
+    sorted_data = []
+    for entry in data:
+        entry = {key: entry[key] for key in order if key in entry}
+        sorted_data.append(entry)
 
     return sorted_data
-
-
-    
-# Function to convert JSON data to a Markdown table
-def convert_table(json_data, mapping, table_name):
-    # Extracting headers
-    headers = ["name", "description", "links"]
-    alignments = {"name": ":---", "description": ":---", "code": ":---:", "problem_type": ":---:", "model_type": ":---:", "energy_assets": ":---:", "scale": ":---:", "links": ":---:"}
-
-    include_headers = ["name", "description", "code", "links"]
-    alignments = [alignments[header] for header in include_headers]
-
-    display_headers = convert_string(include_headers)
-
-    header_row = '| ' + ' | '.join(display_headers) + ' |'
-    separator_row = '| ' + ' | '.join(alignments) + ' |'
-
-    # Extracting rows
-    rows = []
-    for idx_entry, entry in enumerate(json_data):
-        row = "|"
-        missing_keys = [key for key in headers if key not in entry.keys()]
-        if missing_keys:
-            print(missing_keys)
-            raise ValueError(f"Error in element at index {idx_entry}: Missing keys {missing_keys}")
-        
-        for header in include_headers:
-            if header == "name":
-                row = row + "`" + str(entry[header]) + "`"
-            if header == "description":
-                row = row + str(entry[header])
-            if header in mapping:
-                for idx_key, key in enumerate(entry[header]):
-                    row = row + mapping[header][key]
-                    if idx_key < len(entry[header])-1: row = row + " "
-            if header in ["links"]:
-                for idx_key, key in enumerate(entry[header]):
-                    row = row + "[[" + key + "]]" + "(" + entry[header][key] + ")" 
-                    if idx_key < len(entry[header])-1: row = row + ", "
-            row = row + "|"
-
-        rows.append(row)
-
-    # Combining all parts into the final table
-    markdown_table = '\n'.join([header_row, separator_row] + rows)
-
-    # Save the Markdown table to a file
-    with open(table_name, 'w') as file:
-        file.write(markdown_table)
-
-def insert_table(source_file="model_table.md", read_file="README_no_table.md", target_file="README.md", placeholder="<!-- table_placeholder -->"):
-    with open(source_file, 'r') as f:
-        table_content = f.read()
-
-    with open(read_file, 'r') as f:
-        target_content = f.read()
-
-    new_content = target_content.replace(placeholder, table_content)
-
-    with open(target_file, 'w') as file: 
-        file.write(new_content)
-
-    print("Markdown table inserted into the README.")
-
-def convert_string(old_list): 
-    new_list = []
-    for old_string in old_list:
-        words = old_string.split('_')
-        new_string = ' '.join(word.capitalize() for word in words)
-        new_list.append(new_string)
-
-    return new_list
 
 def write_custom_json(data, name, indent=2):
     def format_dict(d, level=1):
@@ -127,6 +58,76 @@ def write_custom_json(data, name, indent=2):
     with open(name, 'w') as f:
         f.write(output_json)
 
+# Function to convert JSON data to a Markdown table
+def convert_table(json_data, table_name):
+    # Extracting headers
+    headers = ["name", "description", "open_source", "links"]
+    alignments = {"name": ":---", "description": ":---", "open_source": ":---:", "links": ":---:"}
+
+    include_headers = ["name", "description", "open_source", "links"]
+    alignments = [alignments[header] for header in include_headers]
+
+    display_headers = convert_string(include_headers)
+
+    header_row = '| ' + ' | '.join(display_headers) + ' |'
+    separator_row = '| ' + ' | '.join(alignments) + ' |'
+
+    # Extracting rows
+    rows = []
+    for idx_entry, entry in enumerate(json_data):
+        row = "|"
+        missing_keys = [key for key in headers if key not in entry.keys()]
+        if missing_keys:
+            print(missing_keys)
+            raise ValueError(f"Error in element at index {idx_entry}: Missing keys {missing_keys}")
+        
+        for header in include_headers:
+            if header == "name":
+                row = row + "`" + str(entry[header]) + "`"
+            if header == "description":
+                row = row + str(entry[header])
+            if header == "open_source":
+                if entry[header] == True:
+                    row = row + "Yes"
+                else:
+                    row = row + "No"
+            if header in ["links"]:
+                for idx_key, key in enumerate(entry[header]):
+                    row = row + "[[" + key + "]]" + "(" + entry[header][key] + ")" 
+                    if idx_key < len(entry[header])-1: row = row + ", "
+            row = row + "|"
+
+        rows.append(row)
+
+    # Combining all parts into the final table
+    markdown_table = '\n'.join([header_row, separator_row] + rows)
+
+    # Save the Markdown table to a file
+    with open(table_name, 'w') as file:
+        file.write(markdown_table)
+
+def convert_string(old_list): 
+    new_list = []
+    for old_string in old_list:
+        words = old_string.split('_')
+        new_string = ' '.join(word.capitalize() for word in words)
+        new_list.append(new_string)
+
+    return new_list
+
+def insert_table(source_file="model_table.md", read_file="README_no_table.md", target_file="README.md", placeholder="<!-- table_placeholder -->"):
+    with open(source_file, 'r') as f:
+        table_content = f.read()
+
+    with open(read_file, 'r') as f:
+        target_content = f.read()
+
+    new_content = target_content.replace(placeholder, table_content)
+
+    with open(target_file, 'w') as file: 
+        file.write(new_content)
+
+    print("Markdown table inserted into the README.")
 
 if __name__ == "__main__":
     # Read JSON data from a file
@@ -145,10 +146,10 @@ if __name__ == "__main__":
     data_ai_models = sort_keys(data_ai_models, schema_ai_models)
 
     # Write custom JSON file
-    ai_models_json = write_custom_json(data_ai_models, "ai_models.json", indent=2)
+    ai_models_json = write_custom_json(data_ai_models, "data_ai_models.json", indent=2)
 
     # Convert JSON to Markdown table
-    convert_table(data_ai_models, mapping, "ai_models.md")
+    convert_table(data_ai_models, "ai_models.md")
 
     # Insert the Markdown table into a README file
     insert_table()
